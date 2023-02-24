8fb9255bb28429b2309edc27005d6e21c378a244
junaid-o <junaidmohd795@gmail.com>
2023-02-25 04:56:11 +0530

    push

--- .ipynb_checkpoints/untitled-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-files = glob.glob("/*.html")
-html_list = []
-
-for file in files:
-    with open(file, "r") as f:
-        html_list.append(f.read())
-html_str = "\n".join(html_list)
